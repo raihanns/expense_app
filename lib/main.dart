90dde71df6129be86c322c3f6b7a4f1c8c357d68
raihanns <rainsdotnet@gmail.com>
2022-03-01 23:45:56 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -45,16 +45,11 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _userTransactions = [
+    //dummy data
     // Transaction(
     //   id: 't1',
     //   title: 'New Shoes',
     //   amount: 69.99,
-    //   date: DateTime.now(),
-    // ),
-    // Transaction(
-    //   id: 't2',
-    //   title: 'Weekly Groceries',
-    //   amount: 15.99,
     //   date: DateTime.now(),
     // ),
   ];
